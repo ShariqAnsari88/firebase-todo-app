f81f0ea8b5a06e5fa6164a2d0e5bc671e418f9a4
ShariqAnsari88 <36728316+ShariqAnsari88@users.noreply.github.com>
2023-04-07 23:29:34 +0530

    started building main page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-const inter = Inter({ subsets: ["latin"] });
-
+import { AiOutlinePlus } from "react-icons/ai";
 export default function Home() {
-    return <main>lksdjflajflaj;l</main>;
+    return (
+        <main className="">
+            <div className="max-w-xl mx-auto mt-10">
+                <div className="flex flex-col items-center gap-5">
+                    <img src="/todo-banner.jpg" className="w-[400px]" />
+                </div>
+                <div className="flex items-center gap-2 mt-10">
+                    <input
+                        placeholder="What to do Today?"
+                        type="text"
+                        className="font-medium border-[2px] border-black h-[60px] grow shadow-sm rounded-md px-4 focus-visible:outline-yellow-400 placeholder:text-black"
+                        autoFocus
+                    />
+                    <button className="w-[60px] h-[60px] rounded-md bg-green-500 flex justify-center items-center cursor-pointer">
+                        <AiOutlinePlus size={30} color="#fff" />
+                    </button>
+                </div>
+            </div>
+        </main>
+    );
 }
