09ab2387214ea3d31b825bb753d8950de061d2e5
ShariqAnsari88 <36728316+ShariqAnsari88@users.noreply.github.com>
2023-04-07 21:26:44 +0530

    register page added

---
+++ pages/register.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
+const RegisterForm = () => {
+    return (
+        <main className="flex lg:h-[100vh]">
+            <div className="w-full lg:w-[60%] p-8 md:p-14 flex items-center justify-center lg:justify-start">
+                <div className="p-8 w-[600px]">
+                    <h1 className="text-6xl font-semibold">Sign Up</h1>
+                    <p className="mt-6 ml-1">
+                        Already have an account ?{" "}
+                        <span className="underline hover:text-blue-400 cursor-pointer">
+                            Login
+                        </span>
+                    </p>
+
+                    <div className="bg-black/[0.05] text-white w-full py-4 mt-10 rounded-full transition-transform hover:bg-black/[0.8] active:scale-90 flex justify-center items-center gap-4 cursor-pointer group">
+                        <FcGoogle size={22} />
+                        <span className="font-medium text-black group-hover:text-white">
+                            Login with Google
+                        </span>
+                    </div>
+
+                    <div className="mt-10 pl-1 flex flex-col">
+                        <label>Name</label>
+                        <input
+                            type="text"
+                            className="border-b border-black p-4 pt-0 outline-0 focus-within:border-blue-400"
+                        />
+                    </div>
+                    <div className="mt-10 pl-1 flex flex-col">
+                        <label>Email</label>
+                        <input
+                            type="text"
+                            className="border-b border-black p-4 pt-0 outline-0 focus-within:border-blue-400"
+                        />
+                    </div>
+                    <div className="mt-10 pl-1 flex flex-col">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            className="border-b border-black p-4 pt-0 outline-0 focus-within:border-blue-400"
+                        />
+                    </div>
+                    <button className="bg-black text-white w-44 py-4 mt-10 rounded-full transition-transform hover:bg-black/[0.8] active:scale-90">
+                        Sign Up
+                    </button>
+                </div>
+            </div>
+            <div
+                className="w-[40%] bg-slate-400 bg-cover bg-right-top hidden lg:block"
+                style={{
+                    backgroundImage: "url('/login-banner.jpg')",
+                }}
+            ></div>
+        </main>
+    );
+};
+
+export default RegisterForm;
