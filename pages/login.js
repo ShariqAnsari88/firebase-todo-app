3c0ac58165f9e2df3512674153b91bbdfb4b6df0
ShariqAnsari88 <36728316+ShariqAnsari88@users.noreply.github.com>
2023-04-07 21:18:43 +0530

    login page responsiveness completed

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { FcGoogle } from "react-icons/fc";
 const LoginForm = () => {
     return (
-        <main className="flex h-[100vh]">
-            <div className="w-[60%] p-14 flex items-center">
+        <main className="flex lg:h-[100vh]">
+            <div className="w-full lg:w-[60%] p-8 md:p-14 flex items-center justify-center lg:justify-start">
                 <div className="p-8 w-[600px]">
                     <h1 className="text-6xl font-semibold">Login</h1>
                     <p className="mt-6 ml-1">
@@ -11,6 +12,14 @@ const LoginForm = () => {
                             Sign Up
                         </span>
                     </p>
+
+                    <div className="bg-black/[0.05] text-white w-full py-4 mt-10 rounded-full transition-transform hover:bg-black/[0.8] active:scale-90 flex justify-center items-center gap-4 cursor-pointer group">
+                        <FcGoogle size={22} />
+                        <span className="font-medium text-black group-hover:text-white">
+                            Login with Google
+                        </span>
+                    </div>
+
                     <div className="mt-10 pl-1 flex flex-col">
                         <label>Email</label>
                         <input
@@ -31,7 +40,7 @@ const LoginForm = () => {
                 </div>
             </div>
             <div
-                className="w-[40%] bg-slate-400 bg-cover bg-right-top"
+                className="w-[40%] bg-slate-400 bg-cover bg-right-top hidden lg:block"
                 style={{
                     backgroundImage: "url('/login-banner.jpg')",
                 }}
